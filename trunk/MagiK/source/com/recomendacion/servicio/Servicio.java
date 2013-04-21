1bc5617826fc5bae8d650a2c2d877e51e17c72f3
kd.avila143@gmail.com <kd.avila143@gmail.com@2b40777d-b9a0-d1ab-78ea-ff7d435354c1>
2013-04-21 02:16:19 +0000

    Servicio

---
+++ trunk/MagiK/source/com/recomendacion/servicio/Servicio.java
@@ -0,0 +1,14 @@
+package com.recomendacion.servicio;
+
+public class Servicio
+{
+    public String[] getRecomendaciones(String documento)
+    {
+        return new String[]{"Julio","Kelvin","Tita"};
+    }
+    
+    public String[] getPalabrasClave(String documento)
+    {
+        return new String[]{"Julio","Kelvin","Tita"};
+    }
+}
