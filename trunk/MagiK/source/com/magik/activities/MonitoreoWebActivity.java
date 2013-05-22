66842f0480ab328b112879495e5da33e08e3f393
kd.avila143@gmail.com <kd.avila143@gmail.com@2b40777d-b9a0-d1ab-78ea-ff7d435354c1>
2013-05-22 18:57:51 +0000

    aRREGLOS PARA SETTINGS

--- trunk/MagiK/source/com/magik/activities/MonitoreoWebActivity.java
+++ trunk/MagiK/source/com/magik/activities/MonitoreoWebActivity.java
@@ -20,7 +20,7 @@ import com.magik.mundo.data.ControlerData;
 
 public class MonitoreoWebActivity extends Activity
 {
-	private static String CLASE_MONITOREO = "MWEB";
+    private static String CLASE_MONITOREO = "MWEB";
     private ListView atrListView;
     private Button btnStart;
     private Button btnClear;
@@ -28,6 +28,8 @@ public class MonitoreoWebActivity extends Activity
     private SimpleCursorAdapter adapter;
     private ControlerData artData;
     private PersistenceManager pm;
+    private Thread t;
+    private boolean sensorProcess;
 
     @Override
     public void onCreate( Bundle savedInstanceState )
@@ -35,22 +37,22 @@ public class MonitoreoWebActivity extends Activity
         super.onCreate( savedInstanceState );
         pm = new PersistenceManager( this.getApplicationContext( ) );
         setContentView( R.layout.activity_monitoreo_web );
-        artData = new ControlerData();
-        artData.crearFile(CLASE_MONITOREO, "TIME;Numero de consulta;Titulo;Url");
+        artData = new ControlerData( );
+        artData.crearFile( CLASE_MONITOREO, "TIME;Numero de consulta;Titulo;Url" );
         btnStart = ( Button )findViewById( R.id.web_control_start );
         btnClear = ( Button )findViewById( R.id.web_control_clears );
         atrListView = ( ListView )findViewById( R.id.ListWeb_List );
-        inicialiarList( cargarDatosWeb() );
-        inicializarBotones();
+        inicialiarList( cargarDatosWeb( ) );
+        inicializarBotones( );
     }
 
-    private void inicializarBotones()
+    private void inicializarBotones( )
     {
         int screenWidth = this.getWindowManager( ).getDefaultDisplay( ).getWidth( );
         if( !started )
         {
             btnStart.setEnabled( false );
-            cargarDatosLista(cargarDatosWeb( ));
+            cargarDatosLista( cargarDatosWeb( ) );
             started = true;
         }
         btnStart.setOnClickListener( new View.OnClickListener( )
@@ -58,11 +60,32 @@ public class MonitoreoWebActivity extends Activity
             // @Override
             public void onClick( View v )
             {
-                cargarDatosLista(cargarDatosWeb( ));
-                btnStart.setEnabled( false );
-                started = true;
+                t = new Thread( )
+                {
+                    @Override
+                    public void run( )
+                    {
+                        while( sensorProcess )
+                        {
+                            try
+                            {
+                                cargarDatosLista( cargarDatosWeb( ) );
+                                btnStart.setEnabled( false );
+                                started = true;
+                                sleep( 30000 );
+                            }
+                            catch( InterruptedException e )
+                            {
+                                // TODO Auto-generated catch block
+                                e.printStackTrace( );
+                            }
+                        }
+                    }
+                };
+                t.start( );
             }
         } );
+
         btnClear.setWidth( screenWidth / 4 );
         btnClear.setOnClickListener( new View.OnClickListener( )
         {
@@ -76,68 +99,66 @@ public class MonitoreoWebActivity extends Activity
         } );
         refreshLog( );
     }
-    
+
     public void refreshLog( )
     {
-        cargarDatosLista(cargarDatosWeb( ));
+        cargarDatosLista( cargarDatosWeb( ) );
         adapter.notifyDataSetChanged( );
     }
 
     public void clearLog( )
     {
-        String[] values = new String[] { "" };
-        ArrayAdapter<String> adapterVacio = new ArrayAdapter<String>(this,R.layout.rowlist_monitoreo_web, R.id.textlistNombre, values);
+        String[] values = new String[]{ "" };
+        ArrayAdapter<String> adapterVacio = new ArrayAdapter<String>( this, R.layout.rowlist_monitoreo_web, R.id.textlistNombre, values );
         atrListView.setAdapter( adapterVacio );
         adapter = null;
-        
+
     }
-    
-    private void cargarDatosLista(Cursor cursor)
+
+    private void cargarDatosLista( Cursor cursor )
     {
-        
-    	String[] displayFields = { Browser.BookmarkColumns.URL, Browser.BookmarkColumns.TITLE };
+
+        String[] displayFields = { Browser.BookmarkColumns.URL, Browser.BookmarkColumns.TITLE };
         int[] viewFields = { R.id.textlistNombre, R.id.textlistDescription };
         adapter = new SimpleCursorAdapter( this, R.layout.rowlist_monitoreo_web, cursor, displayFields, viewFields );
         atrListView.setAdapter( adapter );
-        
+
     }
-    
-    private void guardarDatos(Cursor cursor)
+
+    private void guardarDatos( Cursor cursor )
     {
-    	artData.existDelete(CLASE_MONITOREO);
-    	String rta ="";
-    	cursor.moveToFirst();
-    	while (cursor.isAfterLast() == false) 
-    	{
-    		String vrTitle=cursor.getString(2);    		
-    		String vrUrl=cursor.getString(1);
-    		if(vrTitle==null)
-    			vrTitle="";    		
-    		if(vrUrl==null)
-    			vrUrl="";
-    		artData.writeToFile( vrTitle + ","  + vrUrl +"\n" , true, CLASE_MONITOREO);
-    		guardarDocumentoBase(vrUrl, vrTitle, PersistenceManager.HTML);
-    		cursor.moveToNext();
-    	}
+        artData.existDelete( CLASE_MONITOREO );
+        String rta = "";
+        cursor.moveToFirst( );
+        while( cursor.isAfterLast( ) == false )
+        {
+            String vrTitle = cursor.getString( 2 );
+            String vrUrl = cursor.getString( 1 );
+            if( vrTitle == null )
+                vrTitle = "";
+            if( vrUrl == null )
+                vrUrl = "";
+            artData.writeToFile( vrTitle + "," + vrUrl + "\n", true, CLASE_MONITOREO );
+            guardarDocumentoBase( vrUrl, vrTitle, PersistenceManager.HTML );
+            cursor.moveToNext( );
+        }
     }
-    
-    private void guardarDocumentoBase(String documentName, String documentTitle, String documentType)
+
+    private void guardarDocumentoBase( String documentName, String documentTitle, String documentType )
     {
         try
         {
-            if( !pm.isFileInTable(documentName) )
+            if( !pm.isFileInTable( documentName ) )
             {
                 pm.createDocument( documentName, documentType, documentTitle );
             }
         }
         catch( Exception e )
         {
-            e.printStackTrace();
+            e.printStackTrace( );
         }
     }
 
-    
-    
     private void inicialiarList( Cursor cursor )
     {
         atrListView.setOnItemClickListener( new OnItemClickListener( )
@@ -145,20 +166,18 @@ public class MonitoreoWebActivity extends Activity
             public void onItemClick( AdapterView<?> parent, View view, int position, long id )
             {
                 TextView view2 = ( TextView )view.findViewById( R.id.textlistDescription );
-                 Toast.makeText( getApplicationContext( ), view2.getText( ), Toast.LENGTH_SHORT ).show( );
+                Toast.makeText( getApplicationContext( ), view2.getText( ), Toast.LENGTH_SHORT ).show( );
             }
         } );
-        cargarDatosLista(cursor);
+        cargarDatosLista( cursor );
     }
-    
-    private Cursor cargarDatosWeb()
+
+    private Cursor cargarDatosWeb( )
     {
         String[] selection = { Browser.BookmarkColumns._ID, Browser.BookmarkColumns.URL, Browser.BookmarkColumns.TITLE };
-        Cursor cursor = getContentResolver( ).query( Browser.BOOKMARKS_URI, selection, null, null,  Browser.BookmarkColumns.DATE + " DESC LIMIT 30" );
-        //startManagingCursor( cursor );
+        Cursor cursor = getContentResolver( ).query( Browser.BOOKMARKS_URI, selection, null, null, Browser.BookmarkColumns.DATE + " DESC LIMIT 30" );
         guardarDatos( cursor );
         return cursor;
     }
 
-    
 }
