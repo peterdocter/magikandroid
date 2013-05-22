9396563f88ccec15cd25ab49056d3fbd3072957f
kd.avila143 <kd.avila143@gmail.com>
2013-05-22 18:57:51 +0000

    aRREGLOS PARA SETTINGS

--- MagiK/source/com/magik/activities/FileModificationMonitorActivity.java
+++ MagiK/source/com/magik/activities/FileModificationMonitorActivity.java
@@ -22,8 +22,6 @@ public class FileModificationMonitorActivity extends Activity
 {
     private Button btn1;
     private Button btn2;
-//    private Button btn3;
-//    private Button btn4;
     private TextView text1;
     private boolean started = false;
     private boolean pmStarted = false;
@@ -32,12 +30,9 @@ public class FileModificationMonitorActivity extends Activity
     public void onCreate( Bundle savedInstanceState )
     {
         super.onCreate( savedInstanceState );
-//        LocalBroadcastManager.getInstance( this ).registerReceiver( mMessageReceiver, new IntentFilter( "action" ) );
         setContentView( R.layout.file_modification_monitor );
         btn1 = ( Button )findViewById( R.id.file_modification_monitor_btn_first );
         btn2 = ( Button )findViewById( R.id.file_modification_monitor_btn_second );
-//        btn3 = ( Button )findViewById( R.id.file_modification_monitor_btn_third );
-//        btn4 = ( Button )findViewById( R.id.file_modification_monitor_btn_forth );
         int screenWidth = this.getWindowManager( ).getDefaultDisplay( ).getWidth( );
         btn1.setWidth( screenWidth / 4 );
         text1 = ( TextView )findViewById( R.id.file_modification_monitor_log );
@@ -73,61 +68,13 @@ public class FileModificationMonitorActivity extends Activity
                 started = false;
             }
         } );
-//        btn3.setWidth( screenWidth / 4 );
-//        btn3.setOnClickListener( new View.OnClickListener( )
-//        {
-//            @Override
-//            public void onClick( View v )
-//            {
-//                text1.setText( "" );// FileAccessLogStatic.accessLogMsg );
-//            }
-//        } );
-//        btn4.setWidth( screenWidth / 4 );
-//        btn4.setOnClickListener( new View.OnClickListener( )
-//        {
-//            @Override
-//            public void onClick( View v )
-//            {
-//                clearLog( );
-//            }
-//        } );
         refreshLog( );
     }
 
-//    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver( )
-//    {
-//        @Override
-//        public void onReceive( Context context, Intent intent )
-//        {
-//            String action = intent.getAction( );
-//            String currentSpeed = intent.getStringExtra( "action" );
-//            Toast.makeText( getApplicationContext( ), currentSpeed, Toast.LENGTH_SHORT ).show( );
-//            startProcessMonitor( );
-//
-//        }
-//    };
-
-//    public void startProcessMonitor( )
-//    {
-//        Intent intent = new Intent(this, ProcessMonitor.class);
-//        startService( intent );
-//        btn1.setEnabled( true );
-//        pmStarted = true;
-//    }
-//
-//    public void stopProcessMonitor( )
-//    {
-//        if( !pmStarted )
-//        {
-//            stopService( new Intent( getApplicationContext( ), ProcessMonitor.class ) );
-//            btn1.setEnabled( true );
-//            pmStarted = true;
-//        }
-//    }
 
     public void refreshLog( )
     {
-        text1.setText( "" );// FileAccessLogStatic.accessLogMsg );
+        text1.setText( "" );
     }
 
     public void clearLog( )
